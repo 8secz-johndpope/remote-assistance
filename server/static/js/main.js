a4e4e81387f019d6018fa195f84178ae0ccd9857
Yulius Tjahjadi <yulius@fxpal.com>
2019-12-16 10:46:43 -0800

    fixed the default url for the customer

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -4,7 +4,8 @@ $(function() {
         var host = ifaces.val();
         var url;
         if (host == 'default') {
-            url = 'https://' + window.location.host + '/customer';
+            var roomid = e.target.getAttribute('data-roomid');
+            url = 'https://' + window.location.host + '/' + roomid + '/customer';
         } else {
             url = 'https://' + host + ':5443/customer';
         }
@@ -12,5 +13,5 @@ $(function() {
         $('#url').text(url);
     }
     $('#interfaces').on('change', onChangeInterfaces);
-    onChangeInterfaces();
+    onChangeInterfaces({target: document.getElementById('default-value')});
 });
