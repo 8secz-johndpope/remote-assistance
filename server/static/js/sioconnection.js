fd3f4b035aef21f0af0f0c4ff42e2af2c2e59d18
Yulius Tjahjadi <yulius@fxpal.com>
2019-12-19 13:38:26 -0800

    fix javascript error preventing firefox from connecting

--- server/static/js/sioconnection.js
+++ server/static/js/sioconnection.js
@@ -11,7 +11,9 @@ SIOConnection.prototype.setupSocket = function() {
     if (SIOConnection.socket) {
         var socket = SIOConnection.socket;
         socket.on('frame', function(data) {
-            connection.handleData(data);
+            if (data.hands) {
+                connection.handleData(data);
+            }
         });
         return socket;
     } else {
