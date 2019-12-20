f0d4d0a2e970f465e13c3c3b5bd182a780d3e190
Scott Carter <carter@fxpal.com>
2019-12-20 08:40:21 -0800

    Adding sketch

--- server/config/default.js
+++ server/config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
     databaseUrl: process.env.DATABASE_URL || 'mysql://ace:hmqN3kCmawG33whCnEGy65PUi@harddb.fxpal.net/ace',
-    host: process.env.HOST || '127.0.0.1',
+    host: process.env.HOST || '0.0.0.0',
     port: process.env.PORT || 5443,
     wsport: 3000,
     clipLoc: "stor/"
