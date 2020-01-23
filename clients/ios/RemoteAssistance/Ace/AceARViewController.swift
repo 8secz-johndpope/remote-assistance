9389b0e3f2c6a5582053ab876c081c5b08db488e
Yulius Tjahjadi <yulius@fxpal.com>
2020-01-23 15:41:24 -0800

    add images for 2d anchors & split out code for ar view

--- clients/ios/RemoteAssistance/Ace/AceARViewController.swift
+++ clients/ios/RemoteAssistance/Ace/AceARViewController.swift
@@ -149,7 +149,25 @@ class AceARViewController : UIViewController {
         self.arView.delegate = self
         self.arView.session.delegate = self
     }
-
+    
+    @IBAction func onToggleVR(_ sender: Any) {
+        // Toggle VR view
+        if let vrVC = self.vrVC {
+            self.arView.isHidden = false
+            vrVC.view.removeFromSuperview()
+            vrVC.removeFromParent()
+            self.vrVC = nil
+        } else {
+            self.arView.isHidden = true
+            let vc = AceVRViewController.instantiate(fromAppStoryboard: .Ace)
+            vc.sceneView = self.arView
+            vc.view.frame = self.view.frame
+            vc.viewDidLoad()
+            self.view.addSubview(vc.view)
+            self.addChild(vc)
+            self.vrVC = vc
+        }
+    }
 }
 
 extension AceARViewController: WRTCClientDelegate {
@@ -233,298 +251,6 @@ extension AceARViewController: ARSessionDelegate {
                 self.capturer.captureFrame(image)
             }
             self.lastTimeStamp = now
-        }
-    }
-}
-
-// AR SCreen
-
-extension AceARViewController {
-    
-    func initScreenAR() {
-        let overlayContent = self.loadHTML()
-        self.webView = UIWebView(frame:CGRect(x:0,y:0,width: 640, height:480))
-        //self.webView = WKWebView(frame:CGRect(x:0,y:0,width: 640, height:480))
-        self.webView?.isOpaque = false
-        self.webView?.backgroundColor = UIColor.clear
-        self.webView?.scrollView.backgroundColor = UIColor.clear
-        self.webView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.webView?.loadHTMLString(overlayContent, baseURL: nil)
-    }
-
-    func loadHTML() -> String
-    {
-        do {
-            if let filepath = Bundle.main.path(forResource: "overlay", ofType: "html") {
-                let overlayContent = try String(contentsOfFile: filepath)
-                return overlayContent
-            }
-        } catch {
-            return ""
-        }
-        return ""
-    }
-    
-    func imageWithBorderPoints(image: UIImage, points: [CGPoint]) -> UIImage?
-    {
-        let size = CGSize(width: image.size.width, height: image.size.height)
-        UIGraphicsBeginImageContext(size)
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        image.draw(in: rect, blendMode: .normal, alpha: 1.0)
-        let context = UIGraphicsGetCurrentContext()
-        context?.setStrokeColor(red: 0, green: 0, blue: 0, alpha: 1)
-        //context?.setLineWidth(8)
-        context?.stroke(rect, width: 8)
-        context?.setFillColor(red: 255, green: 255, blue: 255, alpha: 1)
-
-        /*let tl = points[0]
-        let tr = points[1]
-        let bl = points[2]
-        let br = points[3]*/
-
-        let tl = points[2]
-        let tr = points[3]
-        let bl = points[0]
-        let br = points[1]
-
-        let barheight: CGFloat = 16
-        // encode x positions of tl and tr on the top border
-        context?.fill(CGRect(x: tl.x-4, y: 0, width: 8, height: barheight))
-        context?.fill(CGRect(x: tr.x-4, y: 0, width: 8, height: barheight))
-        // encode x positions of bl and br on the bottom border
-        context?.fill(CGRect(x: bl.x-4, y: size.height, width: 8, height: -barheight))
-        context?.fill(CGRect(x: br.x-4, y: size.height, width: 8, height: -barheight))
-        // encode y positions of tl and bl on the left border
-        context?.fill(CGRect(x: 0, y: tl.y-4, width: barheight, height: 8))
-        context?.fill(CGRect(x: 0, y: bl.y-4, width: barheight, height: 8))
-        // encode y positions of tr and br on the right border
-        context?.fill(CGRect(x: size.width, y: tr.y-4, width: -barheight, height: 8))
-        context?.fill(CGRect(x: size.width, y: br.y-4, width: -barheight, height: 8))
-
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
-    @IBAction func onResetScreenAR(_ sender: UIButton) {
-        DispatchQueue.main.async {
-            self.arView.session.pause()
-            self.rectangleNodes.forEach({ $1.removeFromParentNode() })
-            self.rectangleNodes.removeAll()
-            self.arView.scene.rootNode.enumerateChildNodes { (node, stop) in
-                node.removeFromParentNode()
-            }
-            self.arView.session.run(self.configuration, options: [.removeExistingAnchors, .resetTracking])
-        }
-    }
-    
-    @IBAction func onToggleVR(_ sender: Any) {
-        // Toggle VR view
-        if let vrVC = self.vrVC {
-            self.arView.isHidden = false
-            vrVC.view.removeFromSuperview()
-            vrVC.removeFromParent()
-            self.vrVC = nil
-        } else {
-            self.arView.isHidden = true
-            let vc = AceVRViewController.instantiate(fromAppStoryboard: .Ace)
-            vc.sceneView = self.arView
-            vc.view.frame = self.view.frame
-            vc.viewDidLoad()
-            self.view.addSubview(vc.view)
-            self.addChild(vc)
-            self.vrVC = vc
-        }
-    }
-}
-
-extension AceARViewController {
-    
-    class PointerSet {
-        var pos = CGPoint(x:0, y:0)
-        var size = CGSize(width: 0, height: 0)
-        
-        func parse(_ data:[String:CGFloat]) -> PointerSet {
-            if let x = data["x"] {
-                pos.x = x
-            }
-            if let y = data["y"] {
-                pos.y = y
-            }
-            if let cW = data["w"] {
-                size.width = cW
-            }
-            if let cH = data["h"] {
-                size.height = cH
-            }
-            return self
-        }
-        
-        // reframe the start and end points to the phone display space
-        func transformToFrame(_ frameSize: CGSize) -> PointerSet {
-            let aspectRatio = frameSize.width/frameSize.height
-            var scale:CGFloat = 1.0
-            var offset = CGPoint(x: 0, y: 0)
-            
-            // if true, width is filled at the expert side
-            var spanWidth = false
-
-            if (size.width > size.height) {
-                if (frameSize.width > frameSize.height) {
-                    spanWidth = true
-                } else {
-                    spanWidth = false
-                }
-            } else {
-                if (frameSize.width > frameSize.height) {
-                    spanWidth = false
-                } else {
-                    spanWidth = true
-                }
-            }
-            
-            if spanWidth {
-                scale = frameSize.width/size.width
-                offset.x = 0
-                offset.y = -(size.height - size.width/aspectRatio)/2
-            } else {
-                scale = frameSize.height/size.height
-                offset.x = -(size.width - size.height*aspectRatio)/2
-                offset.y = 0
-            }
-            
-            // transform to screen space
-            pos.x = (offset.x + pos.x)*scale
-            pos.y = (offset.y + pos.y)*scale
-            
-            return self
-        }
-    }
-
-    
-    func initARPointer() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(onTap))
-        self.view.addGestureRecognizer(tap)
-        self.arrowObject = AceVirtualObject.object(byName: "Arrow.scn")
-        
-        // animate
-        let animation = CABasicAnimation(keyPath: "rotation")
-        animation.fromValue = NSValue(scnVector4: SCNVector4(x: 0, y: 1, z: 0, w: 0))
-        animation.toValue = NSValue(scnVector4: SCNVector4(x: 0, y: 1, z: 0, w: Float(CGFloat.pi*2)))
-        animation.duration = 3.0
-        animation.autoreverses = false
-        animation.repeatCount = .infinity
-        self.arrowObject?.addAnimation(animation, forKey: "spinAround")
-        
-        // listen for pointer messages
-        let socket = SocketIOManager.sharedInstance
-        socket.on("pointer_set") { data, ack in
-            for line in data {
-                let msg = PointerSet()
-                    .parse(line as! [String:CGFloat])
-                    .transformToFrame(self.view.frame.size)
-                self.setArrow(msg.pos)
-            }
-        }
-
-        socket.on("pointer_clear") { data, ack in
-            self.removeArrow()
-        }
-
-    }
-    
-    @objc func onTap(_ gesture: UITapGestureRecognizer) {
-        print("onTap from AceARViewController")
-        
-        let touchLocation = gesture.location(in: arView)
-        setArrow(touchLocation)
-    }
-    
-    func setArrow(_ point:CGPoint) {
-        if let object = self.arrowObject {
-            setDown(object, basedOn: point)
-        }
-    }
-    
-    func removeArrow() {
-        if let object = self.arrowObject {
-            removeAnchor(object)
-        }
-    }
-    
-    func setDown(_ object: AceVirtualObject, basedOn screenPos: CGPoint) {
-        object.stopTrackedRaycast()
-        
-        // Prepare to update the object's anchor to the current location.
-        object.shouldUpdateAnchor = true
-        
-        // Attempt to create a new tracked raycast from the current location.
-        if let query = arView.raycastQuery(from: screenPos, allowing: .estimatedPlane, alignment: object.allowedAlignment),
-            let raycast = self.createTrackedRaycastAndSet3DPosition(of: object, from: query) {
-            object.raycast = raycast
-        } else {
-            // If the tracked raycast did not succeed, simply update the anchor to the object's current position.
-            object.shouldUpdateAnchor = false
-            self.updateQueue.async {
-                self.addOrUpdateAnchor(for: object)
-            }
-        }
-    }
-    
-    func createTrackedRaycastAndSet3DPosition(of virtualObject: AceVirtualObject, from query: ARRaycastQuery,
-                                              withInitialResult initialResult: ARRaycastResult? = nil) -> ARTrackedRaycast? {
-        if let initialResult = initialResult {
-            self.setTransform(of: virtualObject, with: initialResult)
-        }
-        
-        return arView.session.trackedRaycast(query) { (results) in
-            self.setVirtualObject3DPosition(results, with: virtualObject)
-        }
-    }
-    
-    // - Tag: ProcessRaycastResults
-    private func setVirtualObject3DPosition(_ results: [ARRaycastResult], with virtualObject: AceVirtualObject) {
-        
-        guard let result = results.first else {
-            fatalError("Unexpected case: the update handler is always supposed to return at least one result.")
-        }
-        
-        self.setTransform(of: virtualObject, with: result)
-        
-        // If the virtual object is not yet in the scene, add it.
-        if virtualObject.parent == nil {
-            self.arView.scene.rootNode.addChildNode(virtualObject)
-            virtualObject.shouldUpdateAnchor = true
-        }
-        
-        if virtualObject.shouldUpdateAnchor {
-            virtualObject.shouldUpdateAnchor = false
-            self.updateQueue.async {
-                self.addOrUpdateAnchor(for: virtualObject)
-            }
-        }
-    }
-    
-    func setTransform(of virtualObject: AceVirtualObject, with result: ARRaycastResult) {
-        virtualObject.simdWorldTransform = result.worldTransform
-    }
-    
-    func addOrUpdateAnchor(for object: AceVirtualObject) {
-        // If the anchor is not nil, remove it from the session.
-        if let anchor = object.anchor {
-            arView.session.remove(anchor: anchor)
-        }
-        
-        // Create a new anchor with the object's current transform and add it to the session
-        let newAnchor = ARAnchor(transform: object.simdWorldTransform)
-        object.anchor = newAnchor
-        arView.session.add(anchor: newAnchor)
-    }
-
-    func removeAnchor(_ object: AceVirtualObject) {
-        if let anchor = object.anchor {
-            arView.session.remove(anchor: anchor)
-            object.removeFromParentNode()
         }
     }
 }
