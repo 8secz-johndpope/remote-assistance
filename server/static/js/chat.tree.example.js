d4e11ba05819f6a35e728a926654814afa8043ce
Scott Carter <carter@fxpal.com>
2020-02-05 15:40:03 -0800

    Adding chat documentation

--- server/static/js/chat.tree.example.js
+++ server/static/js/chat.tree.example.js
@@ -3,12 +3,14 @@ const CHAT_TREE_JSON = `
     {
 	"id": 1,
 	"q": "Question text 1?",
-	"next": [2,3]
+	"next": [2,3],
+	"nextLabels": ["1","2"]
 	},
 	{
 	"id": 2,
 	"q": "Question text 2?",
-	"next": [3,4]
+	"next": [3,4],
+	"nextLabels": ["Ready","I need assistance"]
 	},
 	{
 	"id": 3,
