f2894140aaaab8005041381d2a4b360c676d343c
Yulius Tjahjadi <yulius@fxpal.com>
2020-05-06 17:40:49 -0700

    Remove confusing continue button

--- server/static/js/chat.js
+++ server/static/js/chat.js
@@ -442,7 +442,7 @@ function onOCRScanned(scannedText)
   } else {
     nextBtn.type = "ocrResponse"; nextBtn.action = savedAction; nextBtn.actionLabel = scannedText;
     botMsgText = 'I found: <b>' + scannedText + 
-                   '</b>. If that is not correct, please type in the text manually. Otherwise, press <span class="btn btn-primary btn-sm">Continue</span>.';
+                   '</b>. If that is not correct, please type in the text manually. Otherwise, press';
   }
   botResponseArr.push(nextBtn);
   appendMessage(BOT_NAME, BOT_IMG, "left", botMsgText, botResponseArr);
